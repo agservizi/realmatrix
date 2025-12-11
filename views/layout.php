b9dd25c3f858bbf3f19223eb7db769ce5810f197
agservizi <ag.servizi16@gmail.com>
2025-12-11 02:56:31 +0100

    feat: Aggiungi layout e stili per la navigazione e il pannello principale

--- views/layout.php
+++ views/layout.php
@@ -12,29 +12,36 @@ $csrf = hash_hmac('sha256', session_id(), $config['csrf']['secret'] ?? '');
     <link rel="stylesheet" href="/public/assets/css/main.css">
   <meta name="csrf-token" content="<?php echo htmlspecialchars($csrf, ENT_QUOTES); ?>">
 </head>
-<body>
-<nav class="navbar is-dark" role="navigation">
+<body class="has-background-light">
+<nav class="navbar is-dark topbar" role="navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="/dashboard">RealMatrix</a>
   </div>
-  <div class="navbar-menu">
-    <div class="navbar-start">
-      <a class="navbar-item" href="/dashboard">Dashboard</a>
-      <a class="navbar-item" href="/immobili">Immobili</a>
-      <a class="navbar-item" href="/clienti">Clienti</a>
-      <a class="navbar-item" href="/collaboratori">Collaboratori</a>
-      <a class="navbar-item" href="/sharing">Home Sharing</a>
-    </div>
-    <div class="navbar-end">
-      <div class="navbar-item"><button id="logoutBtn" class="button is-light">Logout</button></div>
-    </div>
+  <div class="navbar-end pr-4">
+    <div class="navbar-item"><button id="logoutBtn" class="button is-light">Logout</button></div>
   </div>
 </nav>
-<section class="section">
-  <div class="container">
-    <?php include $templatePath; ?>
-  </div>
-</section>
+
+<div class="layout-shell container is-fluid">
+  <aside class="menu sidebar">
+    <p class="menu-label">Navigazione</p>
+    <ul class="menu-list">
+      <li><a href="/dashboard">Dashboard</a></li>
+      <li><a href="/immobili">Immobili</a></li>
+      <li><a href="/clienti">Clienti</a></li>
+      <li><a href="/collaboratori">Collaboratori</a></li>
+      <li><a href="/sharing">Home Sharing</a></li>
+    </ul>
+  </aside>
+  <main class="main-panel">
+    <section class="section">
+      <div class="container is-fluid">
+        <?php include $templatePath; ?>
+      </div>
+    </section>
+  </main>
+</div>
+
 <script type="module" src="/public/assets/js/main.js"></script>
 </body>
 </html>
